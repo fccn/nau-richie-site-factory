c1742c41709b68b68efb3bdb82c45fbfa427b446
Ivo Branco <ivo.branco@fccn.pt>
2021-09-03 10:38:36 +0100

    🐛(log) prints stack trace if code blocks
    Add a Gunicort worker abort handler that prints the current stack trace
    when the worker timeout's or blocks on waiting for an external
    dependency.

--- docker/files/usr/local/etc/gunicorn/app.py
+++ docker/files/usr/local/etc/gunicorn/app.py
@@ -14,3 +14,16 @@ accesslog = "-"
 # Using '-' for the error log file makes gunicorn log errors to stderr
 errorlog = "-"
 loglevel = "info"
+
+# Handler that forces Gunicorn to print the current stack trace, when its worker timeout’s.
+# This can happen when a request hangs waiting for an external service dependency, like when a
+# database is too slow or can connect to the S3 Bucket provider. The worker receives the SIGABRT
+# signal from the Gunicorn parent process and this handler runs before the worker it stopped.
+# Reference:
+# https://stackoverflow.com/questions/15442361/dump-django-stack-trace-on-gunicorn-timeout
+def worker_abort(worker):
+    import traceback, io
+    debug_info = io.StringIO()
+    debug_info.write("Traceback at time of timeout:\n")
+    traceback.print_stack(file=debug_info)
+    worker.log.critical(debug_info.getvalue())
