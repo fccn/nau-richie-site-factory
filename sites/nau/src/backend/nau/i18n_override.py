91c7d45473c4eae75779ef26a70eab14f6988f6b
Sandro Costa <sandro.costa@fccn.pt>
2021-09-29 12:02:41 +0100

    💄(frontend) add template to override style
    Add new templates to customize Richie's theme.

--- sites/nau/src/backend/nau/i18n_override.py
+++
@@ -1,8 +0,0 @@
-"""
-NAU i18n override keys.
-"""
-from django.utils.translation import gettext_lazy as _
-
-def i18n_override_keys():
-    # remove footer key phrase because NAU has it on the logo!
-    _("Life-changing learning!")
