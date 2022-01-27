8f16579bd6820c33f96d2af5a7cc6d76678d04db
Ivo Branco <ivo.branco@fccn.pt>
2022-01-27 16:28:53 +0000

    ✨(tracking) add facebook pixel tracking code
    Add Meta Facebook Pixel tracking code javascript. GN-764

---
+++ sites/nau/src/backend/nau/facebook_pixel/context_processors.py
@@ -0,0 +1,14 @@
+"""
+Jira Service desk widget context processor
+"""
+from django.conf import settings
+
+
+def facebook_pixel_setting(request):
+    """
+    Facebook Pixel context processor
+    """
+    context = {}
+    if hasattr(settings, "FACEBOOK_PIXEL_ID"):
+        context["FACEBOOK_PIXEL_ID"] = getattr(settings, "FACEBOOK_PIXEL_ID")
+    return context
