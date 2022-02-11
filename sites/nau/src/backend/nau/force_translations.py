f0132d07c1e9f76fc5430c8e53d9bb06bd93e84e
Ivo Branco <ivo.branco@fccn.pt>
2022-02-11 10:02:46 +0000

    🌐(i18n) change translation
    Change translations of 'Persons' and 'First session' for PT and EN.

--- sites/nau/src/backend/nau/force_translations.py
+++ sites/nau/src/backend/nau/force_translations.py
@@ -1,3 +1,7 @@
 from django.utils.translation import gettext_lazy as _
 
+# File where we can include strings to be forcefully changed for NAU
+
 _("{!s} settings")
+_("First session")
+_("Persons")
