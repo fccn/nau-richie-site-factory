36b737d9d22969a9df5c7640eba9cd160ffd5f03
Ivo Branco <ivo.branco@fccn.pt>
2022-07-27 15:05:37 +0100

    🎉(cacademy) init cacademy
    Initialize new C-Academy site for CNCS

---
+++ sites/cacademy/src/backend/cacademy/force_translations.py
@@ -0,0 +1,7 @@
+from django.utils.translation import gettext_lazy as _
+
+# File where we can include strings to be forcefully changed for C-academy
+
+_("{!s} settings")
+_("First session")
+_("Persons")
