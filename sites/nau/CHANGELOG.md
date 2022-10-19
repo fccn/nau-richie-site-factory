147ac7bff4e94db0020ea457198f44ef27cf68af
Ivo Branco <ivo.branco@fccn.pt>
2022-10-19 17:08:12 +0100

    🌐(i18n) fix share course mailto
    Share course on email reference Richie instead of NAU.
    GN-992
    FAN-91

--- sites/nau/CHANGELOG.md
+++ sites/nau/CHANGELOG.md
@@ -20,6 +20,7 @@ Versioning](https://semver.org/spec/v2.0.0.html).
 
 ### Fixed
 
+- 🌐(i18n) fix share course mailto references Richie instead of NAU
 - 🐛(scss) enroll button is not clickable on small width screens
 - 🐛(scss) persons category tags have the wrong hover color
 - 🐛(scss) fix issues with background on icon tag
--- sites/nau/src/backend/nau/force_translations.py
+++ sites/nau/src/backend/nau/force_translations.py
@@ -5,3 +5,21 @@ from django.utils.translation import gettext_lazy as _
 _("{!s} settings")
 _("First session")
 _("Persons")
+
+_("Sub categories")
+_("Related courses")
+_("Courses pagination")
+_("See all courses")
+_("Related organizations")
+_("Related organizations pagination")
+_("Related blogposts")
+_("Related blogposts pagination")
+_("Related persons")
+_("Related persons pagination")
+_("No other open course runs")
+_("No open course runs")
+_("Life-changing learning!")
+
+# Richie apps/core/templates/social-networks/course-badges.html
+_("Follow a course online with Richie")
+_('I just enrolled to the course "%(title)s" on Richie: %(url)s')
