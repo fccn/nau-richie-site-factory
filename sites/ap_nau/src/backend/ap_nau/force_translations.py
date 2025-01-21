49e2c5eee0fff8d3065c95aa44ef57ab2ae1b98a
Tiago-Salles <tiagosalles20@gmail.com>
2025-01-21 17:05:26 +0000

    🎉(ap) started the site ap_nau
    - Added new site to public administration employees
    in accordance with the first requirements in terms of
    UI/UX aspects

---
+++ sites/ap_nau/src/backend/ap_nau/force_translations.py
@@ -0,0 +1,20 @@
+"""
+NAU translations
+"""
+
+from django.utils.translation import gettext_lazy as _
+
+# File where we can include strings to be forcefully changed for NAU
+
+_("{!s} settings")
+_("First session")
+_("Persons")
+_("Life-changing learning!")
+
+# Richie apps/core/templates/social-networks/course-badges.html
+_("Follow a course online with Richie")
+_('I just enrolled to the course "%(title)s" on Richie: %(url)s')
+
+# Richie apps/core/templates/social-networks/blogpost-badges.html
+_("Richie news: %(title)s")
+_("Richie news: %(title)s %(url)s")
---
+++ sites/ap_nau/src/backend/ap_nau/tests/test_google_tag_manager.py
@@ -0,0 +1,115 @@
+"""
+End-to-end tests for the course detail view
+"""
+
+from django.test.utils import override_settings
+
+from cms.test_utils.testcases import CMSTestCase
+from richie.apps.courses.factories import CourseFactory
+
+
+# pylint: disable=duplicate-code
+class GoogleTagManagerBaseTemplateRenderingCMSTestCase(CMSTestCase):
+    """
+    Test case that verifies if the Google Tag Manager is being rendered and not using the Richie
+    upstream Web Analytics.
+    """
+
+    @override_settings(
+        WEB_ANALYTICS={"google_tag_manager": {"tracking_id": "xpto-key"}}
+    )
+    def test_template_base_google_tag_manager_present(self):
+        """
+        Tests if the Google Tag Manager code is added if the GOOGLE_TAG_MANAGER_ID setting is
+        present.
+        """
+        course = CourseFactory()
+        page = course.extended_object
+        page.publish("en")
+
+        url = course.extended_object.get_absolute_url()
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertContains(
+            response,
+            "www.googletagmanager.com/gtm.js?id='+i+dl+'';",
+        )
+        self.assertContains(
+            response,
+            "dataLayer','xpto-key'",
+        )
+        self.assertContains(
+            response,
+            '"https://www.googletagmanager.com/ns.html?id=xpto-key"',
+        )
+        self.assertContains(
+            response,
+            "xpto-key",
+        )
+
+    @override_settings(
+        GOOGLE_TAG_MANAGER_ID="GTM-SOME-KEY",
+        WEB_ANALYTICS={
+            "google_tag_manager": {
+                "tracking_id": "GTM-SOME-KEY",
+                "environment": "&gtm_auth=cexSLlJmC6wAalbsw6AuQA&gtm_preview=env-77&gtm_cookies_win=x",  # noqa pylint: disable=line-too-long
+            }
+        },
+    )
+    def test_template_base_google_tag_manager_present_with_environment_config(self):
+        """
+        Tests if the Google Tag Manager code is added if the environment configuration is
+        present.
+        """
+
+        course = CourseFactory()
+        page = course.extended_object
+        page.publish("en")
+
+        url = course.extended_object.get_absolute_url()
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertContains(
+            response,
+            "www.googletagmanager.com/gtm.js",
+        )
+        self.assertContains(
+            response,
+            "dataLayer','GTM-SOME-KEY'",
+        )
+        self.assertContains(
+            response,
+            '"https://www.googletagmanager.com/ns.html?id=GTM-SOME-KEY&gtm_auth=cexSLlJmC6wAalbsw6AuQA&gtm_preview=env-77&gtm_cookies_win=x"',  # noqa pylint: disable=line-too-long
+        )
+        self.assertContains(
+            response,
+            "GTM-SOME-KEY",
+        )
+        self.assertContains(
+            response,
+            "https://www.googletagmanager.com/gtm.js?id='+i+dl+'&gtm_auth=cexSLlJmC6wAalbsw6AuQA&gtm_preview=env-77&gtm_cookies_win=x';",  # noqa pylint: disable=line-too-long
+        )
+
+    def test_template_base_google_tag_manager_absent(self):
+        """
+        Tests if the Google Tag Manager code is not added if the GOOGLE_TAG_MANAGER_ID setting is
+        not present.
+        """
+        course = CourseFactory()
+        page = course.extended_object
+        page.publish("en")
+
+        url = course.extended_object.get_absolute_url()
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertNotContains(
+            response,
+            "www.googletagmanager.com/gtm.js",
+        )
+        self.assertNotContains(
+            response,
+            '"https://www.googletagmanager.com/ns.html"',
+        )
